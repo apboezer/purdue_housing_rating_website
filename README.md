58795f8beb3fe33da1fc1f52c53dc2388d703af5
apboezer <111909744+apboezer@users.noreply.github.com>
2023-01-20 22:00:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# purdue_housing_rating_website
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-mehety)
---
+++ src/app/newPage/newPage.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newPage',
+  templateUrl: './newPage.component.html',
+  styleUrls: ['./newPage.component.css'],
+})
+export class NewPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
